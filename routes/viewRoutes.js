226036f013d153320ac097c697dc9573659f8b49
Jon C <jondotcourtney@gmail.com>
2024-09-11 20:27:39 -0400

    account page (nonfunctional)

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -18,17 +18,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-// router.use((req, res, next) => {
-//   res.setHeader(CSP, POLICY);
-//   next();
-// });
-
-router.use(authController.isLoggedIn);
-
-router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
 //login
-router.get('/login', viewsController.getLoginForm);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
