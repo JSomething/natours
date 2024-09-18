79e4c5eb706bd8e52d85f37e8efaa8b9e00bc53a
Jon C <jondotcourtney@gmail.com>
2024-09-17 20:02:46 -0400

    updating name/email from user page works now. added axios

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 // cookie parser
 app.use(cookieParser());
 
